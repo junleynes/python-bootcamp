0723d8c53140f14595f8bef1f5bcd5efdd732f45
junleynes <junleynes@gmail.com>
2026-06-27 19:14:19 +0800

    Day 1

--- day_01/05_lab_session/exercise/02_positive_input.py
+++ day_01/05_lab_session/exercise/02_positive_input.py
@@ -1,16 +1,21 @@
 # TODO: Ask the user for an input that should be a number
 
 # TODO: Then try to convert this into an integer using the following:
-try:
-    number = input("Enter number: ")
-    number_converted = int(number)
-    print(number)
-    print("You entered a valid number")
-# The user could provide an invalid integer input (string)
-# TODO: Handle this case
-except ValueError:
-    print ("Enter a valid number")
-# The user could give a negative number
-# TODO: Handle this case
+def askinput():
+    while True:
+        try:
+            number = input("Enter number: ")
+            number_converted = int(number)
+            print(number)
+            if number_converted < 0:
+                raise ValueError()
+        # The user could provide an invalid integer input (string)
+        # TODO: Handle this case
+        except ValueError:
+            print ("Enter a valid number")
+        # The user could give a negative number
+        # TODO: Handle this case
 
-# Challenge: TODO: Give the user infinite times to retry
+        # Challenge: TODO: Give the user infinite times to retry
+
+askinput()
